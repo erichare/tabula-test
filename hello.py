a9b771b83d9ef74673e90b57bf05539854d2ed8c
Eric Hare <ericrhare@gmail.com>
2022-08-04 09:01:42 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
 import tabula
 import streamlit as st
 
-def hello(name = "World"):  
-    return "Hello " + str(name) + ", from the Daisi platform"
+def main():
+  st.title(“PDF table extraction with Tabula”)
+  with st.sidebar:
+    uploaded_file = st.file_uploader(“Upload PDF”, type=[“pdf”])
+  file_name = uploaded_file.name
+  with st.spinner(“Extracting with Tabula...“):
+    dfs = tabula.read_pdf(uploaded_file, pages=‘all’)
+  for df in dfs:
+    st.dataframe(df)
+
+if __name__ == “__main__“:
+  main()
