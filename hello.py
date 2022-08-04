d5d3512772b5f84ebe3640284af8c14466e964e8
Eric Hare <ericrhare@gmail.com>
2022-08-04 09:08:51 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,14 +2,18 @@ import tabula
 import streamlit as st
 
 def main():
-  st.title(“PDF table extraction with Tabula”)
-  with st.sidebar:
-    uploaded_file = st.file_uploader(“Upload PDF”, type=[“pdf”])
-  file_name = uploaded_file.name
-  with st.spinner(“Extracting with Tabula...“):
-    dfs = tabula.read_pdf(uploaded_file, pages=‘all’)
-  for df in dfs:
-    st.dataframe(df)
+    st.title("PDF table extraction with Tabula")
+    
+    with st.sidebar:
+        uploaded_file = st.file_uploader("Upload PDF", type=["pdf"])
 
-if __name__ == “__main__“:
-  main()
+    file_name = uploaded_file.name
+
+    with st.spinner("Extracting with Tabula..."):
+        dfs = tabula.read_pdf(uploaded_file, pages='all')
+
+    for df in dfs:
+        st.dataframe(df)
+
+if __name__ == "__main__":
+    main()
